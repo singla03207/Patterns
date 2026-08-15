9958f064f999769d7005f0c6477e3c5f71366052
singla03207 <singlasakshi2007@gmail.com>
2026-08-15 19:13:09 +0530

    Implement pattern printing based on user input
    This program takes multiple inputs and prints a pattern of letters based on the input number.

---
+++ 07_pattern.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+void printp(int n){
+    
+    for (int i = 0; i < n; i++)
+    {
+        char s='A';
+       for (int j =0; j <=i; j++)
+       {
+        cout<<s;
+        s++;
+       }
+       cout<<"\n";
+       
+    }
+}
+int main(){
+    int s;
+    cin>>s;
+    for (int i = 0; i < s; i++)
+    {
+        int n;
+        cin>>n;
+        printp(n);
+    }
+  return 0;
+    
+}
+
+// input- 1 5
+// output--
+// A
+// AB
+// ABC
+// ABCD
+// ABCDE
